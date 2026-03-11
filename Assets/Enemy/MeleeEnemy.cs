15c57abb80b7c3b1de4fd642fb25fd45b9f62deb
Kieran - L101.C3.R3 <1035688@student.sae.edu.au>
2026-03-12 10:43:25 +1100

    ENEMY BAREBONES
    Allowed my enemyAI to move and chase the player, yes this is in the wrong section, will merge it to the main branch for further usage as this barebones piece of code is perfectly harmless.

---
+++ Assets/Enemy/MeleeEnemy.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class MeleeEnemy : MonoBehaviour
+{
+
+    [SerializeField] private float enemySpeed = 4f; // Speed of the enemy
+    [SerializeField] private float attackRange = 5f; // How far the enemy can attack
+    [SerializeField] private float attackCooldown = 2.5f; // Time being attacks
+
+    [SerializeField] private float lastAttackInstance; // Last time that the enemy attacked
+    [SerializeField] public Transform player;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        if (distance > attackRange)
+        {
+            ChasePlayer(); // If in
+        }
+        else
+        {
+            Attack();
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.position, enemySpeed * Time.deltaTime); // Calculates the speed of the AI chasing using the pre-determined enemySpeed
+    }
+
+    private void Attack()
+    {
+        if (Time.time >= lastAttackInstance + attackCooldown)
+        {
+            Debug.Log("The EnemyAI of Melee Class is Attacking! THIS IS A TEST MESSAGE");
+
+            lastAttackInstance = Time.time;
+        }
+    }
+
+
+}
