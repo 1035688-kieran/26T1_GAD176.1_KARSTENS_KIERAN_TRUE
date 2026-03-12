c2f6e33b1e3b4a798797e81aeb518a637a29c289
Kieran - L101.C3.R3 <1035688@student.sae.edu.au>
2026-03-12 12:24:21 +1100

    Added Damage Feedback + imported sources
    Added damage feedback between the player and enemy, and also some commenting/sources

--- Assets/Enemy/MeleeEnemy.cs
+++ Assets/Enemy/MeleeEnemy.cs
@@ -6,43 +6,49 @@ public class MeleeEnemy : MonoBehaviour
     [SerializeField] private float enemySpeed = 4f; // Speed of the enemy
     [SerializeField] private float attackRange = 5f; // How far the enemy can attack
     [SerializeField] private float attackCooldown = 2.5f; // Time being attacks
+    [SerializeField] private int damage = 10; // Damage the enemy does to the player
 
     [SerializeField] private float lastAttackInstance; // Last time that the enemy attacked
     [SerializeField] public Transform player;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] public int maxEnemyHealth = 50;
+    [SerializeField] public int curEnemyHealth;
+
     void Start()
     {
-
+        curEnemyHealth = maxEnemyHealth; // Enemy healths starts at the maximum just like the player
     }
 
     // Update is called once per frame
-    private void Update()
+    void Update()
     {
+        if (player == null) return;
+
         float distance = Vector3.Distance(transform.position, player.position);
 
         if (distance > attackRange)
         {
-            ChasePlayer(); // If in
+            ChasePlayer(); // If not in attack range, will start chase function
         }
         else
         {
-            Attack();
+            Attack(); // This function is connected to my damage functions
         }
 
-        FacePlayer();
+        FacePlayer(); // Will use Vectors to track the location of the player and ""Face"" them
     }
 
-    private void ChasePlayer()
+    private void ChasePlayer() // Tracking function
     {
         transform.position = Vector3.MoveTowards(transform.position, player.position, enemySpeed * Time.deltaTime); // Calculates the speed of the AI chasing using the pre-determined enemySpeed
     }
 
-    private void Attack()
+    private void Attack() // Attack function including damage 
     {
         if (Time.time >= lastAttackInstance + attackCooldown)
         {
             Debug.Log("The spooky EnemyAI of Melee Class is probably attacking! oo very scawy THIS IS A TEST MESSAGE");
-            PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
+            playerStats playerHealth = player.GetComponent<playerStats>(); // Connects back to my player stats/health script
 
             if (playerHealth != null)
             {
@@ -54,16 +60,41 @@ public class MeleeEnemy : MonoBehaviour
         }
     }
 
-    private void FacePlayer()
+    public void TakeDamage(int damage)
+    {
+        curEnemyHealth -= damage;
+
+        Debug.Log("Wow you managed to land a hit!! It has uhhhh, how much health left?" + curEnemyHealth);
+
+        if (curEnemyHealth <= 0)
+        {
+            SpontaneousCombustion2(); // This is my EnemyDie command
+        }
+    }
+
+    private void FacePlayer() // Function so that the object looks at the player
     {
         Vector3 direction = player.position - transform.position;
         direction.y = 0;
 
-        if (direction != Vector3.zero)
+        if (direction != Vector3.zero) // Direction is dead centre
         {
             transform.rotation = Quaternion.LookRotation(direction);
         }
     }
 
+    private void SpontaneousCombustion2()
+    {
+        Destroy(gameObject); // Removes the enemy from the scene
+    }
 
 }
+
+////// HELPFUL SOURCES THAT I AM USING
+/// VECTOR AND QUATERNION STUFF
+// https://docs.unity3d.com/ScriptReference/Vector3.Distance.html DISTANCE VECTOR AND EXPLANATION
+// https://docs.unity3d.com/ScriptReference/Vector3.MoveTowards.html MOVE TOWARDS VECTOR FOR MY ENEMY CHASING MY PLAYER
+// https://docs.unity3d.com/ScriptReference/Quaternion.LookRotation.html HOW TO MAKE MY ENEMY LOOK IN THE DIRECTION OF MY PLAYER FOR THE FACEPLAYER FUNCTION
+///
+
+
--- Assets/Player/playerStats.cs
+++ Assets/Player/playerStats.cs
@@ -1,9 +1,20 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class playerStats : MonoBehaviour
 {
     [SerializeField] private int maxHealth = 100; // Max health of the PLAYER, might use this script for inheritence
     [SerializeField] private int curHealth; // Variable controlling the current health of the player
+    [SerializeField] private float attackRange; // Range of attack for player
+    [SerializeField] private int damage;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Keypad1))
+        {
+            PlayerAttack();
+        }
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,19 +30,35 @@ public class playerStats : MonoBehaviour
 
         if (curHealth <= 0)
         {
-            spontaneousCombustion(); // This is my "Die" command
+            SpontaneousCombustion(); // This is my "Die" command
         }
     }
 
-    private void spontaneousCombustion()
+    private void SpontaneousCombustion()
     {
         Debug.Log("you died!!! RIP"); // will add code to end game
         Time.timeScale = 0f; // Sets the time scale to 0, effectively freezing the game, and being a game-over
     }
 
-    // Update is called once per frame
-    void Update()
+    private void PlayerAttack() 
     {
-        
-    }
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, attackRange)) // Where the raycast starts, and where the raycast ends as well as the range
+        {
+            MeleeEnemy enemyHealth = hit.collider.GetComponent<MeleeEnemy>();
+
+            if (enemyHealth != null)
+            {
+                enemyHealth.TakeDamage(damage); // Enemy takes damage from the takedamage function above
+            }
+        }
+    } 
+
 }
+
+////// HELPFUL SOURCES THAT I AM USING
+/// 
+// https://docs.unity3d.com/ScriptReference/Physics.Raycast.html RAYCAST GENERAL INFORMATION AND USAGE
+// https://docs.unity3d.com/ScriptReference/Time-timeScale.html TIME SCALE, WHICH CAN HELP SPEED UP OR SLOW DOWN AN APPLICATION, ROBERT PUT IT BEST "IT IS BASICALLY TIME TRAVEL"
+// https://www.youtube.com/watch?v=sPiVz1k-fEs YOUTUBE VIDEO FOR GENERAL COMBAT AND HEALTH STATS (BRACKEYS)
+//////
